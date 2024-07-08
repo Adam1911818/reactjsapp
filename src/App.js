158ec7629d6ad0570c59a857174a7d6590cdd387
Adam1911818 <adam1911818@gmail.com>
2024-07-08 11:00:39 +0000

    intial commint

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { fetchData } from './api';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const articles = await fetchData();
+        setData(articles);
+      } catch (error) {
+        console.error('Error fetching data', error);
+        setError('Error fetching data. Please try again later.');
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>strapi and reactjs</h1>
+        {error ? (
+          <p>{error}</p>
+        ) : (
+          <ul>
+            {data.map((item) => (
+              <li key={item.id}>
+                <h2>{item.attributes.title}</h2>
+                <p>{item.attributes.content}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </header>
     </div>
   );
